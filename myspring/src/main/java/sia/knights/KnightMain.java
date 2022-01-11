1175904526029fd608e4e80f0942c417d2625640
Radon <13798358565@163.com>
2022-01-11 13:59:23 +0800

    JavaConfig模式配置bean

--- myspring/src/main/java/sia/knights/KnightMain.java
+++ myspring/src/main/java/sia/knights/KnightMain.java
@@ -1,11 +1,25 @@
 package sia.knights;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sia.knights.config.KnightConfig;
 
 public class KnightMain {
 
     public static void main(String[] args) {
+        loadFromXml();
+        loadFromJavaConfig();
+    }
+
+    public static void loadFromXml() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+        Knight knight = context.getBean(Knight.class);
+        knight.embarkOnQuest();
+        context.close();
+    }
+
+    public static void loadFromJavaConfig() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
